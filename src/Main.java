31d016ccd17aa462cfe1391da118fac1bbe1e5e6
Lucas Vieira <ls6182315@gmail.com>
2023-09-12 15:00:16 -0300

    Initial

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+        System.out.println("Welcome to java!");
+
+        for (int i = 1; i <= 8;i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
